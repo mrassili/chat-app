f928f390bbb0f117e4650f840a1b06a77ae69899
Marouane Rassili <mrassili@aol.com>
2019-04-19 21:43:25 +0100

    Finish overall layout

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,29 @@
 // npm packages needed:
+// (eslint is installed globally)
+// eslint-config-prettier
+// eslint-plugin-jsx-a11y
+// eslint-plugin-import
+// eslint-plugin-promise
+// eslint-plugin-react
+// prettier
 
 module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:jsx-a11y/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:promise/recommended",
     "prettier",
     "prettier/react",
   ],
-  plugins: ["react", "jsx-a11y"],
+  plugins: ["react", "jsx-a11y", "import", "node", "promise"],
   rules: {
     "no-console": "warn",
 
     // react plugin - options
     "react/prop-types": 0,
-    "react/jsx-uses-react": "error",
     "react/jsx-uses-vars": "error",
   },
   parser: "babel-eslint",
--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react"
+import UsersList from "./UsersList"
+import ChatWindow from "./ChatWindow"
+import NewMessage from "./NewMessage"
+import "./reset.css"
+import "./App.css"
 
 class App extends Component {
   render() {
-    return null
+    return (
+      <div className="container">
+        <main className="main-window">
+          <UsersList />
+          <ChatWindow />
+          <NewMessage />
+        </main>
+      </div>
+    )
   }
 }
 
