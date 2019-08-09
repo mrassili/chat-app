603b5ab7bf3a9804112e0f9ef986cdc31f919b50
Marouane Rassili <mrassili@aol.com>
2019-08-09 22:13:22 +0100

    Move api key to env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,23 @@
 import firebase from "firebase/app"
 import "firebase/database"
 
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID,
+} = process.env
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAvXXYDmw5IKJP-Ok3_oMWuI88qC8Oup1k",
-  authDomain: "chat-app-6f94a.firebaseapp.com",
-  databaseURL: "https://chat-app-6f94a.firebaseio.com",
-  projectId: "chat-app-6f94a",
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
   storageBucket: "",
-  messagingSenderId: "149109708516",
-  appId: "1:149109708516:web:e0317c46a0f9dbbe",
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
 }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
